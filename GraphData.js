c1ba5cf0c3605e0d494597b2a612f1424ec219c3
Shannon Murphy <Murphy1693@gmail.com>
2022-11-19 00:00:28 -0800

    Graph data structure added

---
+++ GraphData.js
@@ -0,0 +1,27 @@
+export default class GraphData {
+  constructor() {
+    this._nodes = {};
+    this._links = [];
+  }
+
+  addNode = (node) => {
+    this._nodes[node.id] = this._nodes[node.id] || { ...node, links: {} };
+  };
+
+  addLink = (fromNode, toNode) => {
+    this._nodes[fromNode].hasLink = true;
+    this._nodes[fromNode].links[toNode] = {
+      id: toNode,
+      target: true,
+    };
+    this._nodes[toNode].hasLink = true;
+    this._nodes[toNode].links[fromNode] = {
+      id: fromNode,
+      target: false,
+    };
+    this._links.push({
+      source: this._nodes[fromNode],
+      target: this._nodes[toNode],
+    });
+  };
+}
