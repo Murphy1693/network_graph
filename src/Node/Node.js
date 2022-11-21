b63167441080dbd14f3ad934363eec0986c6054a
Shannon <Murphy1693@gmail.com>
2022-11-21 13:05:30 -0800

    makeRGBA

--- src/Node/Node.js
+++ src/Node/Node.js
@@ -2,6 +2,20 @@ import React from "react";
 import NodeHeader from "./NodeHeader.js";
 import NodeBody from "./NodeBody.js";
 
+const makeRGBA = function (color, opacity = 0.2) {
+    if (color === "red") {
+      return `rgba(255, 0, 0, ${opacity})`;
+    } else if (color === "blue") {
+      return `rgba(0, 0, 255, ${opacity})`;
+    } else if (color === "green") {
+      return `rgba(0, 255, 0, ${opacity})`;
+    } else if (color === "purple") {
+      return `rgba(128, 0, 128, ${opacity})`;
+    } else if (color === "orange") {
+      return `rgba(255, 165, 0, ${opacity})`;
+    }
+  };
+
 const Node = (props) => {
   let nodeStyle = {
     // border: `1px solid ${props.color}`,
@@ -10,10 +24,13 @@ const Node = (props) => {
     flexDirection: "column",
     width: "fit-content",
     padding: "5px 10px 10px 10px",
+    backgroundColor: makeRGBA(props.color, 0.15),
+
   };
 
   return (
     <div style={nodeStyle} className="node">
+        {/* <div style={{backgroundColor: makeRGBA(props.color, 0.15)}}> */}
       <NodeHeader page={props.page} id={props.node.id}></NodeHeader>
       <NodeBody
         page={props.page}
@@ -24,6 +41,7 @@ const Node = (props) => {
             : props.node.display_observed_genotype
         }
       ></NodeBody>
+    {/* </div> */}
     </div>
   );
 };
